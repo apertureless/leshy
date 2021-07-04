b5fe20545e7300d632cc9d0714e527e6e185c94b
Jakub Juszczak <netghost03@gmail.com>
2021-07-04 20:41:54 +0200

    feat: Add title screen

--- World/World.gd
+++ World/World.gd
@@ -3,5 +3,9 @@ extends Node2D
 onready var player := $Player
 
 func _unhandled_input(event: InputEvent) -> void:
+	
 	if event.is_action_pressed("debug_spawn"):
 		get_tree().reload_current_scene()
+		
+	if event.is_action_pressed("exit"):
+		get_tree().change_scene("res://Scenes/TitleScreen.tscn")
