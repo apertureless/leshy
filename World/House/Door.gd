d028d5eac2aa1a72ee51000c441769734fac4711
Jakub Juszczak <netghost03@gmail.com>
2021-07-07 08:45:06 +0200

    feat: Add door und new spirtes

--- World/House/Door.gd
+++ World/House/Door.gd
@@ -1,11 +1,11 @@
+class_name Door
 extends Area2D
 
 export(PackedScene) var target_scene
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("move_up"):
-		print("move up")
-		print(get_overlapping_bodies().size())
+		print("input")
 		if target_scene and get_overlapping_bodies().size() > 0:
 			next_level(target_scene)
 			
